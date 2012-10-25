e8e7a8cabc0bb00dbcd41089e217351348dda7f6
Ilker Kesen <ilkerksn@gmail.com>
2012-10-25 04:57:21 +0300

    make skeleton

---
+++ tavla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* STRUCT DEFINITIONS */
+
+/* FUNCTION DECLERATIONS */
+
+/* MAIN FUNCTION*/
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
+
+/* FUNCTION DEFINITIONS */
