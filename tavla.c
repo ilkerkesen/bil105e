1e18e05de2ae6b915fb990f05337900be2b06929
Ilker Kesen <ilkerksn@gmail.com>
2012-10-27 08:09:52 +0300

    it is easier to understand without piece struct

--- tavla.c
+++ tavla.c
@@ -9,44 +9,34 @@
 
 /* STRUCT DEFINITIONS */
 
-typedef struct {
-  int index;
-  const char* color;
-  int location;
-} piece;
-
 /* FUNCTION DECLERATIONS */
 
-void initialize_pieces(piece* const, const char* []);
-void initialize_locations(int [], int []);
-void update_locations(piece* const, int [], int []);
-void initialize_state(char [][BOARD_COLUMNS]);
-void update_state(char [][BOARD_COLUMNS], int [], int[], int*);
+void initialize_pieces(int [], int []);
+void initialize_board(char [][BOARD_COLUMNS]);
+void update_board(char [][BOARD_COLUMNS], int [], int[], int*);
+void print_board(char [][BOARD_COLUMNS], int*);
 
 /* MAIN FUNCTION*/
 
 int main(int argc, char *argv[])
 {
   /* VARIABLE DEFINITIONS */
-  piece pieces[PIECES];
-  int black_pieces_locations[26];
-  int white_pieces_locations[26];
-  char board_state[BOARD_LINES][BOARD_COLUMNS];
+  int black_pieces[26];
+  int white_pieces[26];
+  char board[BOARD_LINES][BOARD_COLUMNS];
   int blank_lines;
 
   const char *color[] = {"black", "white"};
 
-  initialize_pieces(pieces, color);
-  initialize_locations(black_pieces_locations, white_pieces_locations);
-  initialize_state(board_state);
-  update_locations(pieces, black_pieces_locations, white_pieces_locations);
-  update_state(board_state, black_pieces_locations, white_pieces_locations, &blank_lines);
+  initialize_pieces(black_pieces, white_pieces);
+  initialize_board(board);
+  update_board(board, black_pieces, white_pieces, &blank_lines);
 
 
   int i, j;
   for(i = 0; i < BOARD_LINES; i++) {
     for(j = 0; j < BOARD_COLUMNS; j++)
-      printf("%c [%2d, %2d] ", board_state[i][j]);
+      printf("%c [%2d, %2d] ", board[i][j]);
     printf("\n");
   }
 
@@ -57,60 +47,33 @@ int main(int argc, char *argv[])
 
 /* FUNCTION DEFINITIONS */
 
-void initialize_pieces(piece* const pieces, const char* color[])
+void initialize_pieces(int black_pieces[], int white_pieces[])
 {
-  int i;
+  /* initialize pieces */
+  black_pieces[6] = 5;
+  white_pieces[19] = 5;
 
-  for(i = 0; i < PIECES / 2; i++) {
-    /* black piece */
-    pieces[i].index = i + 1;
-    pieces[i].color = color[0];
+  black_pieces[8] = 3;
+  white_pieces[17] = 3;
 
-    /* white piece */
-    pieces[i+15].index = i + 1;
-    pieces[i+15].color = color[1];
+  black_pieces[13] = 5;
+  white_pieces[12] = 5;
 
-    if(i < 2) {
-      pieces[i].location = 24;
-      pieces[i+15].location = 1;
-    }
-    else if(i < 7) {
-      pieces[i].location = 13;
-      pieces[i+15].location = 12;
-    }
-    else if(i < 10) {
-      pieces[i].location = 8;
-      pieces[i+15].location = 17;
-    }
-    else if(i < 15) {
-      pieces[i].location = 6;
-      pieces[i+15].location = 19;
-    }
-  }
+  black_pieces[24] = 2;
+  white_pieces[1] = 2;
 
-}
 
-void initialize_locations(int black_piece_locations[], int white_piece_locations[])
-{
+  /* fill empty locations with zero  */
   int i;
   for(i = 0; i < LOCATIONS; i++) {
-    black_piece_locations[i] = 0;
-    white_piece_locations[i] = 0;
+    if(i != 6 && i != 8 && i != 13 && i != 24) {
+      black_pieces[i] = 0;
+      white_pieces[LOCATIONS - 1 - i] = 0;
+    }
   }
 }
 
-void update_locations(piece* const pieces, int black_pieces_locations[], int white_pieces_locations[])
-{
-  int i;
-  for(i = 0; i < PIECES; i++) {    
-    if(pieces[i].color == "black")
-      black_pieces_locations[pieces[i].location] += 1;
-    else if(pieces[i].color == "white")
-      white_pieces_locations[pieces[i].location] += 1;
-  }
-}
-
-void initialize_state(char board_state[][BOARD_COLUMNS])
+void initialize_board(char board_state[][BOARD_COLUMNS])
 {
   int i, j;
   for(i = 0; i < BOARD_LINES; i++) {
@@ -119,8 +82,7 @@ void initialize_state(char board_state[][BOARD_COLUMNS])
   }
 }
 
-/* FIXME */
-void update_state(char board_state[][BOARD_COLUMNS], int black_pieces_locations[], int white_pieces_locations[], int* blank_lines)
+void update_board(char board_state[][BOARD_COLUMNS], int black_pieces[], int white_pieces[], int* blank_lines)
 {
   int i, j, k, m, n;
   int top_null_chars, bot_null_chars;
@@ -132,17 +94,17 @@ void update_state(char board_state[][BOARD_COLUMNS], int black_pieces_locations[
 
     for(j = LOCATIONS / 2, n = LOCATIONS / 2 - 1, k = 0; j < LOCATIONS - 1; j++, n--, k++) {
       /* top board */
-      if(black_pieces_locations[j] != 0 && i < black_pieces_locations[j])
+      if(black_pieces[j] != 0 && i < black_pieces[j])
 	board_state[i][k] = 'B';
-      else if(white_pieces_locations[j] != 0 && i < white_pieces_locations[j])
+      else if(white_pieces[j] != 0 && i < white_pieces[j])
 	board_state[i][k] = 'W';
       else
 	top_null_chars += 1;
 
       /* bottom board */ 
-      if(black_pieces_locations[n] != 0 && BOARD_LINES - 1 - m < black_pieces_locations[n])
+      if(black_pieces[n] != 0 && BOARD_LINES - 1 - m < black_pieces[n])
 	board_state[m][k] = 'B';
-      else if(white_pieces_locations[n] != 0 && BOARD_LINES - 1 - m < white_pieces_locations[n])
+      else if(white_pieces[n] != 0 && BOARD_LINES - 1 - m < white_pieces[n])
 	board_state[m][k] = 'W';
       else
 	bot_null_chars += 1;
