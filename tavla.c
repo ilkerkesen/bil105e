82268c4c72b3a68201e59cafdc6152ed478e5bad
Ilker Kesen <ilkerksn@gmail.com>
2012-11-24 08:18:30 +0200

    some documentation added

--- tavla.c
+++ tavla.c
@@ -1,3 +1,61 @@
+/*
+ * Tavla (backgammon) game with text-based ui
+ * Author: İlker Kesen
+ * E-mail: keseni _at_ itu.edu.tr
+ * Number: 040100411
+ *
+ * Documentation
+ *   Coding Standarts
+ *     1. Use under_score instead of camelCase.
+ *     2. Set maximum row to 80 character.
+ *     3. Use type *var instead of type* var for pointers.
+ *     4. Indentations must be two spaces.
+ *   
+ *   General Algorithm
+ *     There are five important things you should know:
+ *       1. Game/Board state is stored in two 26-element-sized integer arrays 
+ *          (black_pieces,  white_pieces). 1-24 elements are available locations
+ *          on board, 0 is location for collected black pieces and broken white
+ *          pieces. Therefore, 25 is location for broken black pieces and collected
+ *          white pieces.
+ *       2. Drawing board is a little bit hard to understand, but in my opinion,
+ *          it is well documented in drawing functions.
+ *	 3. is_available_movement is the most crucial function of game, that checks
+ *	    validation of a movement based on backgammon rules and returns 0 or 1.
+ *       4. AI is not developed totally yet, but play_first_move function works
+ *          nice. It plays first available movement in current state.
+ *       5. Hard AI will depend on "state ranking system". I'll explain it later.
+ *
+ *     If you want to get involved about algorithm, just read the code. 
+ *
+ *   Libraries
+ *     stdio
+ *       - printf
+ *       - scanf
+ *     stdlib
+ *       - rand
+ *       - srand
+ *     string
+ *       - strcmp
+ *     time
+ *       - time
+ *
+ *
+ *   Building (I will generate a Makefile in the future)
+ *     on Linux: compile it with gcc or clang (gcc tavla.c or clang tavla.c)
+ *     on Windows: just compile it with your IDE (Dev-C++/mingw32 works fine)
+ *     on Mac: never tried, but gcc/clang are able to build it.
+ *
+ *   Running
+ *     on Linux: /path/to/source/a.out
+ *     on Windows: just click twice
+ * 
+ *   Hacking
+ *     github.com/ilkerkesen/tavla (will be available after homework deadline)
+ */
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,54 +68,82 @@
 
 /* FUNCTION PROTOTYPES */
 
+/* Welcome and goodbye functions: prints information at start/exit */
 void welcome();
 void goodbye();
 
+/* General game functions: options, states and initializations */
 void select_game_type(int *, int *);
 void initialize_pieces(int [], int []);
 void initialize_game(int *, int *, int, int, int *);
+void update_scores(int, int, int *, int *, int *);
 void game_over(int, int, int *);
 
+/* Board drawing functions */
 void print_board(int [], int []);
-int get_maximum_piece(int [], int[]);
-int get_lines_length(int);
 void print_side(const char *);
 void print_info(int, int, int [], int, int, int, int);
 
+/* Utilize functions for board drawing */
+int get_maximum_piece(int [], int[]);
+int get_lines_length(int);
+
+/* Dice functions  */
 void throw_dices(int *, int *);
 void update_movements(int, int, int []);
-int any_movements_exist(int [], int [], int [], const char *);
-int is_available_movement(int [], int [], int [], int, int, const char *);
 
+/* Piece movement control functions */
+int is_available_movement(int [], int [], int [], int, int, const char *);
+int any_movements_exist(int [], int [], int [], const char *);
+
+/* Piece movement action functions */
+void move_piece(int [], int [], int, int, const char *);
+int play_first_move(int [], int [], int [], const char *);
+
+/* Turn control functions */
 void play_turn(int [], int [], int, int, int [], int *, int, int);
 void human_play(int [], int [], int, int, int [], const char *);
 void computer_play_noob(int [], int [], int, int, int [], const char *);
-int play_first_move(int [], int [], int [], const char *);
-void move_piece(int [], int [], int, int, const char *);
-void update_scores(int, int, int *, int *, int *);
 
 /* MAIN FUNCTION */
 
 int main(int argc, char *argv[])
 {
   /* VARIABLE DEFINITIONS */
+  
+  /* arrays that stores game/board state  */
   int black_pieces[26];
   int white_pieces[26];
 
+  /* variables that stores score data */
   int black_score;
   int white_score;
 
-  int dice1, dice2;
-  int turn; /* turn % 2 == 0 means black's turn, turn % 2 == 1 means white's turn */
-  int movements[4]; /* movements rights caused by dice rolling process */
+  /* dice variables */
+  int dice1;
+  int dice2;
+  
+  /* movements rights caused by dice rolling process */
+  int movements[4];
 
+  /* turn % 2 == 0 means black's turn,
+   * turn % 2 == 1 means white's turn */
+  int turn;
+
+  /* variables that store data whether player is human. */
+  /* 0-> player is computer */
+  /* 1-> player is human */
   int is_black_human; 
   int is_white_human;
 
+  /* 0-> do not play, break main game loop */
+  /* 1-> wanna play it again, i like it */
   int play_again;
 
+  /* initialization for randomizing operations */
   srand(time(NULL));
 
+  /* welcome function, writes information about game at start*/
   welcome();
   
   /* main loop */
@@ -68,24 +154,35 @@ int main(int argc, char *argv[])
     /* game initialization */
     initialize_game(&black_score, &white_score, dice1, dice2, &turn);
 
-    /* game loop */
+    /* game loop, it is over when a player's score equals 5 or more */
     while(black_score < 5 && white_score < 5) {
       initialize_pieces(black_pieces, white_pieces);
-      /* subgame loop */
+      
+      /* subgame loop. it finishes when a player collects his all pieces  */
       while(black_pieces[0] != 15 && white_pieces[25] != 15) {
+	/* roll dices and update movement rights list */
 	throw_dices(&dice1, &dice2);
 	update_movements(dice1, dice2, movements);
+
+	/* turn loop, check play_turn function */
 	play_turn(black_pieces, white_pieces, dice1, dice2,
 		  movements, &turn, is_black_human, is_white_human);
       
-      }  
+      }
+      /* update scores when subgame is over */
       update_scores(black_pieces[0], white_pieces[25],
 		    &black_score, &white_score, &turn);
     }
+    /* game over, check game_over function */
     game_over(black_score, white_score, &play_again);
+  
+    /* if user want to play again, then repeat iteration */
   } while(play_again);
 
+  /* goodbye function, check goodbye function */
   goodbye();
+
+  /* return 0, it is succesful */
   return 0;
 }
 
@@ -93,6 +190,11 @@ int main(int argc, char *argv[])
 
 void welcome()
 {
+  /* 
+   * usage: welcome(); 
+   * Prints information about game at start.
+   */
+
   printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
 	 "///////////////////////////////////////////////////",
 	 "//                                               //",
@@ -108,11 +210,61 @@ void welcome()
 
 void goodbye()
 {
+  /*
+   * usage: goodbye();
+   * Prints informaton about game at exit.
+   */
+
   printf("\nThanks for playing.\n");
 }
 
 void select_game_type(int *is_black_human, int *is_white_human)
 {
+  /*
+   * select_game_type (Game Type Selection) function
+   * Gets game options (game mode, color) from user and sets them.
+   *
+   * Input Arguments:
+   *   *is_black_human:
+   *      0 -> Black player is computer.
+   *      1 -> Black player is human.
+   *   *is_white_human:
+   *      0 -> White player is computer.
+   *      1 -> White player is human.
+   *
+   * Variables used in this function:
+   *   game_type:
+   *      0 -> Human vs. Human Game
+   *      1 -> Human vs. Computer Game
+   *      2 -> Computer vs. Computer Game
+   *   color:
+   *      0 -> black
+   *      1 -> white
+   *
+   * Output Arguments:
+   *   NULL
+   *
+   * Algorithm (pythonic psuedo-code):
+   *   get game type from user.    
+   *   while game type is not valid,
+   *       get game type from user.
+   *
+   *   if game type is 0,
+   *       both players are human.
+   *   else if game type is 1,
+   *       get color from user.
+   *       while color is not valid,
+   *           get color from user.
+   *       if color is 0,
+   *           black player is human,
+   *           and white player is computer.
+   *       otherwise,
+   *           white player is human,
+   *           and black player is computer.
+   *   otherwise,
+   *       both players are computers.
+   */
+
   int game_type;
   int color;
 
@@ -123,17 +275,21 @@ void select_game_type(int *is_black_human, int *is_white_human)
 	   "1. Human vs. Computer Game",
 	   "2. Computer vs. Computer Game",
 	   "Enter game type (0, 1 or 2): ");
-
+    
+    /* get game type from user */
     scanf("%d", &game_type);
 
+    /* if it is not valid, print warning */
     if(game_type != 0 && game_type != 1 && game_type != 2)
       printf("Invalid selection. Please try again.\n");
 
     printf("\n");
-
+    
+    /* if it is not valid, repeat it */
   } while(game_type != 0 && game_type != 1 && game_type != 2);
 
   if(game_type == 0) {
+    /* both players are human */
     *is_black_human = 1;
     *is_white_human = 1;
   }
@@ -145,26 +301,32 @@ void select_game_type(int *is_black_human, int *is_white_human)
 	     "1. White",
 	     "Enter color (0 or 1): ");
 
+      /* get color data of human player from user */
       scanf("%d", &color);
 
+      /* if it is wrong, then print warning */
       if(color != 0 && color != 1)
 	printf("Invalid selection. Please try again.\n");
 
       printf("\n");
 
+      /* do it until valid value entered */
     } while(color != 0 && color != 1);
 
     if(color == 0) {
+      /* black is human, white is computer */
       *is_black_human = 1;
       *is_white_human = 0;
     }
     else if(color == 1) {
+      /* black is computer, white is human */
       *is_black_human = 0;
       *is_white_human = 1;
     }
 
   }
   else if(game_type == 2) {
+    /* both players are computer */
     *is_black_human = 0;
     *is_white_human = 0;
   }
@@ -173,6 +335,17 @@ void select_game_type(int *is_black_human, int *is_white_human)
 
 void initialize_pieces(int black_pieces[], int white_pieces[])
 {
+  /* Pieces (board) Initialization Function
+     Initialiazes piece locations.
+
+     Input Arguments:
+       black_pieces[]: integer array
+         - stores location datas of black pieces
+	 - size is 26
+	 - first element stores "number of collected black pieces"
+	 - last element stores "number of broken black pieces"
+
+*/
   /* initialize pieces */
   black_pieces[6] = 5;
   white_pieces[19] = 5;
@@ -199,43 +372,59 @@ void initialize_pieces(int black_pieces[], int white_pieces[])
 void initialize_game(int *black_score, int *white_score,
 		     int dice1, int dice2, int *turn)
 {
-    *black_score = 0;
-    *white_score = 0;
-    
-    do {
-      printf("\nThrowing dices.");
-      throw_dices(&dice1, &dice2);
-      printf("\n%s%d\n%s%d\n",
-	     "  Black: ", dice1,
-	     "  White: ", dice2);
-    } while(dice1 == dice2);
+  /* set scores to zero */
+  *black_score = 0;
+  *white_score = 0;
 
-    if(dice1 > dice2) {
-      *turn = 0;
-      printf("Black");
-    }
-    else {
-      *turn = 1;
-      printf("White");
-    }
+  /* roll dices */
+  do {
+    printf("\nThrowing dices.");
+    throw_dices(&dice1, &dice2);
+    printf("\n%s%d\n%s%d\n",
+	   "  Black: ", dice1,
+	   "  White: ", dice2);
+  } while(dice1 == dice2);
+  
 
-    printf(" starts.\n");
+  if(dice1 > dice2) {
+    /* if dice1 > dice2, then black starts*/
+    *turn = 0;
+    printf("Black");
+  }
+  else {
+    /* if dice1 < dice2, then white starts*/
+    *turn = 1;
+    printf("White");
+  }
+  
+  printf(" starts.\n");
 }
 
 void game_over(int black_score, int white_score, int *play_again)
 {
+  /* write scoreboard */
   printf("\n%s\n  %s%d\n  %s%d\n",
 	 "Game Over.",
 	 "Black: ", black_score,
 	 "White: ", white_score);
 
+
   if(black_score > white_score)
+    /* 
+     * if black score is greater than white 
+     * score, then black player is winner 
+     */
     printf("Black");
   else if(black_score < white_score)
+    /* 
+     * if white score is greater than black 
+     * score, then white player is winner 
+     */
     printf("White");
 
   printf(" player won!\n\n");
 
+  /* get play_again value*/
   printf("If you want to play again, enter 1: ");
   scanf("%d", play_again);
   printf("\n");
@@ -248,6 +437,7 @@ void print_board(int black_pieces[], int white_pieces[])
   
   int limit = get_lines_length(get_maximum_piece(black_pieces, white_pieces));
 
+  /* write top of board */
   print_side("top");
   
   for(i = 0; i < limit; i++) {
@@ -268,6 +458,7 @@ void print_board(int black_pieces[], int white_pieces[])
     printf("\n");
   }
 
+  /* write bottom of board */
   print_side("bottom");
 }
 
@@ -294,6 +485,7 @@ int get_maximum_piece(int black_pieces[], int white_pieces[])
 
 int get_lines_length(int max_piece)
 {
+  /* evalute board line number according to maximum piece */
   if (max_piece < 15)
     return 14;
 
@@ -364,6 +556,7 @@ void print_info(int dice1, int dice2, int movements[], int broken_black,
 
 void throw_dices(int *dice1, int *dice2)
 {
+  /* roll dices */
   *dice1 = rand() % 6 + 1;
   *dice2 = rand() % 6 + 1;
 }
