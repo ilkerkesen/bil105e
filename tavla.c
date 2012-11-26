fcf390bf0e37d80240a2528bad8536544e8da775
Ilker Kesen <ilkerksn@gmail.com>
2012-11-26 03:01:52 +0200

    comments added

--- tavla.c
+++ tavla.c
@@ -54,19 +54,19 @@
  *     github.com/ilkerkesen/tavla (will be available after homework deadline)
  */
 
-
+/* --------------------------- INCLUDED LIBRARIES --------------------------- */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-/* CONSTANT DEFINITIONS */
+/* -------------------------- CONSTANT DEFINITONS --------------------------- */
 
 #define PIECES 30
 #define LOCATIONS 26
 
-/* FUNCTION PROTOTYPES */
+/* -------------------------- FUNCTION PROTOTYPES --------------------------- */
 
 /* Welcome and goodbye functions: prints information at start/exit */
 void welcome();
@@ -105,11 +105,11 @@ void play_turn(int [], int [], int, int, int [], int *, int, int);
 void human_play(int [], int [], int, int, int [], const char *);
 void computer_play_noob(int [], int [], int, int, int [], const char *);
 
-/* MAIN FUNCTION */
+/* ----------------------------- MAIN FUNCTION ----------------------------- */
 
 int main(int argc, char *argv[])
 {
-  /* VARIABLE DEFINITIONS */
+/* ------------------------- VARIABLE DEFINITIONS -------------------------- */
   
   /* arrays that stores game/board state  */
   int black_pieces[26];
@@ -186,14 +186,11 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-/* FUNCTION DEFINITIONS */
+/* ------------------------- FUNCTION DEFINITIONS -------------------------- */
 
 void welcome()
 {
-  /* 
-   * usage: welcome(); 
-   * Prints information about game at start.
-   */
+  /* Prints information about game at start. */
 
   printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
 	 "///////////////////////////////////////////////////",
@@ -210,62 +207,23 @@ void welcome()
 
 void goodbye()
 {
-  /*
-   * usage: goodbye();
-   * Prints informaton about game at exit.
-   */
+  /* Prints informaton about game at exit. */
 
   printf("\nThanks for playing.\n");
 }
 
 void select_game_type(int *is_black_human, int *is_white_human)
 {
-  /*
-   * select_game_type (Game Type Selection) function
-   * Gets game options (game mode, color) from user and sets them.
-   *
-   * Input Arguments:
-   *   *is_black_human:
-   *      0 -> Black player is computer.
-   *      1 -> Black player is human.
-   *   *is_white_human:
-   *      0 -> White player is computer.
-   *      1 -> White player is human.
-   *
-   * Variables used in this function:
-   *   game_type:
-   *      0 -> Human vs. Human Game
-   *      1 -> Human vs. Computer Game
-   *      2 -> Computer vs. Computer Game
-   *   color:
-   *      0 -> black
-   *      1 -> white
-   *
-   * Output Arguments:
-   *   NULL
-   *
-   * Algorithm (pythonic psuedo-code):
-   *   get game type from user.    
-   *   while game type is not valid,
-   *       get game type from user.
-   *
-   *   if game type is 0,
-   *       both players are human.
-   *   else if game type is 1,
-   *       get color from user.
-   *       while color is not valid,
-   *           get color from user.
-   *       if color is 0,
-   *           black player is human,
-   *           and white player is computer.
-   *       otherwise,
-   *           white player is human,
-   *           and black player is computer.
-   *   otherwise,
-   *       both players are computers.
-   */
+  /* Gets game options (game mode, color) from user and sets them. */
 
+  /* 
+   * game_type 0 -> human vs. human 
+   *           1 -> human vs. computer
+   *           2 -> computer vs. computer
+   */
   int game_type;
+  
+  /* color: 0 -> black, 1 -> white */
   int color;
 
   do {
@@ -335,17 +293,8 @@ void select_game_type(int *is_black_human, int *is_white_human)
 
 void initialize_pieces(int black_pieces[], int white_pieces[])
 {
-  /* Pieces (board) Initialization Function
-     Initialiazes piece locations.
+  /* initializes pieces and their locations. */
 
-     Input Arguments:
-       black_pieces[]: integer array
-         - stores location datas of black pieces
-	 - size is 26
-	 - first element stores "number of collected black pieces"
-	 - last element stores "number of broken black pieces"
-
-*/
   /* initialize pieces */
   black_pieces[6] = 5;
   white_pieces[19] = 5;
@@ -372,6 +321,12 @@ void initialize_pieces(int black_pieces[], int white_pieces[])
 void initialize_game(int *black_score, int *white_score,
 		     int dice1, int dice2, int *turn)
 {
+  /* 
+   * initializes main game:
+   *   - set scores to zero
+   *   - decides which player starts
+   */
+
   /* set scores to zero */
   *black_score = 0;
   *white_score = 0;
@@ -387,12 +342,12 @@ void initialize_game(int *black_score, int *white_score,
   
 
   if(dice1 > dice2) {
-    /* if dice1 > dice2, then black starts*/
+    /* if dice1 > dice2, then black starts */
     *turn = 0;
     printf("Black");
   }
   else {
-    /* if dice1 < dice2, then white starts*/
+    /* if dice1 < dice2, then white starts */
     *turn = 1;
     printf("White");
   }
@@ -402,12 +357,13 @@ void initialize_game(int *black_score, int *white_score,
 
 void game_over(int black_score, int white_score, int *play_again)
 {
+  /* prints "game over" */
+
   /* write scoreboard */
   printf("\n%s\n  %s%d\n  %s%d\n",
 	 "Game Over.",
 	 "Black: ", black_score,
 	 "White: ", white_score);
-
 
   if(black_score > white_score)
     /* 
@@ -427,23 +383,55 @@ void game_over(int black_score, int white_score, int *play_again)
   /* get play_again value*/
   printf("If you want to play again, enter 1: ");
   scanf("%d", play_again);
+
   printf("\n");
 }
 
 void print_board(int black_pieces[], int white_pieces[])
 {
-  /* counters  */
+  /* prints current state of board to screen */
+
+  /* counters */
   int i, j, k;
   
+  /* get number of board lines */
   int limit = get_lines_length(get_maximum_piece(black_pieces, white_pieces));
 
   /* write top of board */
   print_side("top");
   
+  /* write every line */
   for(i = 0; i < limit; i++) {
+    /* write left part of board (it is not a location)*/
     printf("||||");
+    
+    /* i know, one variable is enough, but it is more aesthetic */
     for(j = 12, k = 13; j > 0; j--, k++) {
+      /* print spaces for (   B   W       B...)*/
       printf("%3s", "");
+      
+      /* 
+       * first_condition || second_condition
+       *
+       * pieces[k] -> number of pieces in k location
+       * i -> line number 
+       *
+       * first_condition is for top board (locations from 13 to 24)
+       *   - it is not difficult to understand.
+       *   - if number of pieces in k location is greater than line number,
+       *     then write B (or W).
+       *   - ex. let k is 13 and pieces[k] = 5, it will print five times B (or W)
+       *       for i = 0, 1, 2, 3, 4. as you can see, there must be 5 pieces.
+       *
+       * second_condition is for bottom board (locations from 12 to 1)
+       *   - it is a little bit hard to understand, but solution is very practical.
+       *   - if number of pieces in k location plus line number is greater than or
+       *     equal to limit, then write B (or W).
+       *   - ex. (IMPORTANT) let k is 12, pieces[k] = 5 and limit = 14, it will
+       *     print five times B (or W) for i = 9 (5 + 9 >= 14, then print),
+       *     i = 10 (5 + 10 >= 14, then print) ... i = 14 (5 + 14 >= 14, then print)
+       *     as you can see, i = 8 (8 + 5 >= 14, it is not true, do not print)
+       */
 
       if(black_pieces[k] > i || black_pieces[j] + i >= limit)
 	printf("B");
@@ -452,9 +440,12 @@ void print_board(int black_pieces[], int white_pieces[])
       else
 	printf(" ");
 
+      /* print |||| middle of board row and right of board row */
       if(k == 18 || k == 24 || j == 7 || j == 1)
 	printf("%3s||||", "");
     }
+    
+    /* new line */
     printf("\n");
   }
 
@@ -486,19 +477,30 @@ int get_maximum_piece(int black_pieces[], int white_pieces[])
 int get_lines_length(int max_piece)
 {
   /* evalute board line number according to maximum piece */
-  if (max_piece < 15)
+  if (max_piece < 14)
     return 14;
 
+  /* 
+   * + 1 because, let 13th location have 15 black pieces
+   * without + 1, you wont understand locations of all
+   * these pieces, they will be on 12th location, too.
+   * it is possible :)
+   */
+  
   return max_piece + 1;
 }
 
 void print_side(const char *side)
 {
+  /* prints top/bottom part of board, location numbers */
+
   int i;
   
+  /* if side is top */
   if(!strcmp(side, "top")) {
     printf("%40sWhite Home Base\n", "");
     for(i = 13; i < 25; i++) {
+      /* print location numbers */
       if(i % 6 == 1)
 	printf("%6s", "");
       if(i == 19)
@@ -508,7 +510,7 @@ void print_side(const char *side)
     printf("\n");
   }
 
-  /* ||||---|---|.... */
+  /* print ||||---|---|.... like board part */
   for(i = 0; i < 14; i++) {
     if(i % 7 == 0) {
       printf("|||");
@@ -517,9 +519,10 @@ void print_side(const char *side)
   }
   printf("||||\n");
   
-
+  /* if side is bottom */
   if(!strcmp(side, "bottom")) {
     for(i = 12; i > 0; i--) {
+      /* print location numbers */
       if(i % 6 == 0)
 	printf("%6s", "");
       if(i == 6)
@@ -534,6 +537,11 @@ void print_side(const char *side)
 void print_info(int dice1, int dice2, int movements[], int broken_black,
 		int collected_black, int broken_white, int collected_white)
 {
+  /* 
+   * prints all data that do not seem on board
+   * like broken/collected pieces, dices, movements 
+   */
+
   int i;
 
   printf("\n%s%d%s%d%s%d\n%s%d%s%d%s%d%s",
@@ -564,6 +572,7 @@ void throw_dices(int *dice1, int *dice2)
 
 void update_movements(int dice1, int dice2, int movements[])
 {
+  /* get movement rights from rolled dices */
   int i;
   
   if(dice1 == dice2) {
@@ -582,12 +591,26 @@ void update_movements(int dice1, int dice2, int movements[])
 int any_movements_exist(int black_pieces[], int white_pieces[],
 			int movements[], const char *color)
 {
+  /* returns 0 if there are no valid movements, otherwise returns 1 */
+
+  /* 
+   * checks location user has just entered 
+   * if there is no piece on this location
+   * return 0
+   */
+  
   if(movements[0] == 0 && movements[1] == 0 &&
      movements[2] == 0 && movements[3] == 0)
     return 0;
  
+  /* counters  */
   int i, j;
 
+  /* 
+   * check broken black pieces, if there is any valid 
+   * movement for  broken black piece, then return 1
+   */
+  
   if(!strcmp(color, "black") && black_pieces[25] != 0) {
     for(i = 0; i < 4; i++) {
       if(is_available_movement(black_pieces, white_pieces,
@@ -596,6 +619,11 @@ int any_movements_exist(int black_pieces[], int white_pieces[],
 	return 1;
     }
   }
+
+  /* 
+   * check broken white pieces, if there is any valid
+   * movement for broken white piece, then return 1
+   */
 
   if(!strcmp(color, "white") && white_pieces[0] != 0) {
     for(i = 0; i < 4; i++) {
@@ -606,6 +634,11 @@ int any_movements_exist(int black_pieces[], int white_pieces[],
     }
   }
 
+  /*
+   * if there is no broken pieces, then check other pieces
+   * if there is a valid movement, then return 1
+   */
+  
   for(i = 1; i < LOCATIONS - 1; i++) {
     for(j = 0; j < 4; j++) {
       if(is_available_movement(black_pieces, white_pieces, 
@@ -639,12 +672,20 @@ int is_available_movement(int black_pieces[], int white_pieces[],
 
     /* black collecting */
     else if(location - movement < 1) {
-      
+      /* if there is a piece out of black home, return 0 */
       for(i = 7; i < LOCATIONS; i++) {
 	if(black_pieces[i] != 0)
 	  return 0;
       }
       
+      /* 
+       * it is for that scenario.
+       * black is collecting but dices are 6 and 6.
+       * there is no piece at 6th location.
+       * but there are pieces on 5th location.
+       * than player can move the piece on 5th location.
+       */
+
       if(movement > location) {
 	for(i = location + 1; i < 7; i++) {
 	  if(black_pieces[i] != 0)
@@ -670,10 +711,19 @@ int is_available_movement(int black_pieces[], int white_pieces[],
 
     /* white collecting */
     else if(location + movement > 25) {
+      /* if there is a piece out of white home, return 0 */
       for(i = 18;  i > -1; i--) {
 	if(white_pieces[i] != 0)
 	  return 0;
       }
+
+      /* 
+       * it is for that scenario.
+       * white is collecting but dices are 6 and 6.
+       * there is no piece at 18th location.
+       * but there are pieces on 19th location.
+       * than player can move the piece on 19th location.
+       */
 
       if(location + movement > 24) {
 	for(i = location - 1; i > 18; i--) {
@@ -696,6 +746,8 @@ void play_turn(int black_pieces[], int white_pieces[],
 	       int dice1, int dice2, int movements[],
 	       int *turn, int is_black_human, int is_white_human)
 {
+  /* decides who is playing */
+
   if(is_black_human == 1 && *turn % 2 == 0) {
     human_play(black_pieces, white_pieces,
 	       dice1, dice2, movements, "black");
@@ -722,13 +774,16 @@ void play_turn(int black_pieces[], int white_pieces[],
 void human_play(int black_pieces [], int white_pieces [], int dice1,
 		int dice2, int movements [], const char *color)
 {
-  /* counter */
+  /* turn function for human players */
+
+  /* counters */
   int i;
 
   /* user input variables */
   int location;
   int movement;
 
+  /* if there is no available movement, than tell it to player */
   if(!any_movements_exist(black_pieces, white_pieces, movements, color)) {
     print_board(black_pieces, white_pieces);
     print_info(dice1, dice2, movements,
@@ -737,6 +792,7 @@ void human_play(int black_pieces [], int white_pieces [], int dice1,
     printf("%s's turn. No available movement.\n\n", color);
   }
 
+  /* while there is a valid movement, then let player play */
   while(any_movements_exist(black_pieces, white_pieces, movements, color)) {
     print_board(black_pieces, white_pieces);
     print_info(dice1, dice2, movements,
@@ -750,15 +806,19 @@ void human_play(int black_pieces [], int white_pieces [], int dice1,
       printf("  Enter movement: ");
       scanf("%d", &movement);
 
+      /* if user input is not valid, warn player */
       if(!is_available_movement(black_pieces, white_pieces, 
 				movements, location, movement, color))
 	printf("Invalid movement. Please try again.\n\n");
 
+      /* if it is not a valid movement, than repeat it */
     } while(!is_available_movement(black_pieces, white_pieces, movements,
 				   location, movement, color));
     
+    /* move piece */
     move_piece(black_pieces, white_pieces, location, movement, color);
 
+    /* set played movement value to zero */
     for(i = 0; i < 4; i++) {
       if(movements[i] == movement) {
 	movements[i] = 0;
@@ -772,6 +832,8 @@ void human_play(int black_pieces [], int white_pieces [], int dice1,
 void computer_play_noob(int black_pieces [], int white_pieces [], int dice1,
 			int dice2, int movements [], const char *color)
 {
+  /* computer plays his turn and computer is a total noob. */
+
   /* counter */
   int i, j, k;
 
@@ -779,6 +841,7 @@ void computer_play_noob(int black_pieces [], int white_pieces [], int dice1,
   int location;
   int movement;
 
+  /* if there is no valid movements, than tell it to user */
   if(!any_movements_exist(black_pieces, white_pieces, movements, color)) {
     print_board(black_pieces, white_pieces);
     print_info(dice1, dice2, movements,
@@ -787,11 +850,13 @@ void computer_play_noob(int black_pieces [], int white_pieces [], int dice1,
     printf("%s's turn. No available movement.\n\n", color);
   }
 
+  /* while there is valid movement */
   while(any_movements_exist(black_pieces, white_pieces, movements, color)) {
     print_board(black_pieces, white_pieces);
     print_info(dice1, dice2, movements,
 	       black_pieces[25], black_pieces[0],
 	       white_pieces[0], white_pieces[25]);
+    /* find a valid movement and play it */
     play_first_move(black_pieces, white_pieces, movements, color);
   }
 }
@@ -799,13 +864,20 @@ void computer_play_noob(int black_pieces [], int white_pieces [], int dice1,
 int play_first_move(int black_pieces[], int white_pieces[],
 		    int movements[], const char *color)
 {
+  /* plays first valid movement */
+
+  /* counters */
   int i, j, k;
   
   printf("%s's turn.\n", color);
 
+  /* check all locations */
   for(i = 0, j = 25; i < LOCATIONS; i++, j--) {
+    /* check all movements */
     for(k = 0; k < 4; k++) {
+      /* if computer player is black */
       if(!strcmp(color, "black")) {
+	/* if the movement is valid */
 	if(is_available_movement(black_pieces, white_pieces, movements,
 				 j, movements[k], color) && movements[k] != 0) {
 	  printf("  Enter location: %d\n", j);
@@ -815,7 +887,9 @@ int play_first_move(int black_pieces[], int white_pieces[],
 	  return 1;
 	}
       }
+      /* if computer player is white */
       else if(!strcmp(color, "white")) {
+	/* if the movement is valid */
 	if(is_available_movement(black_pieces, white_pieces, movements,
 				 i, movements[k], color) && movements[k] != 0) {	
 	  printf("  %s: %d\n  %s: %d\n",
@@ -835,13 +909,22 @@ int play_first_move(int black_pieces[], int white_pieces[],
 void move_piece(int black_pieces [], int white_pieces [],
 		int location, int movement, const char *color)
 {
+  /* moves piece */
+
+  /* for black pieces */
   if(!strcmp(color, "black")) {
+    /* decrease piece number of location of selected piece (-1) */
     black_pieces[location] -= 1;
+    
+    /* if player is collecting (see is_available_movement function )*/
     if(movement > location) {
       black_pieces[0] += 1;
     }
     else {
+      /* increase piece number of new location */
       black_pieces[location - movement] += 1;
+
+      /* if there is an enemy piece, then hit it */
       if(white_pieces[location - movement] == 1) {
 	white_pieces[location - movement] = 0;
 	white_pieces[0] += 1;
@@ -849,13 +932,20 @@ void move_piece(int black_pieces [], int white_pieces [],
     }
   }
   
+  /* for white pieces */
   if(!strcmp(color, "white")) {
+    /* decrease piece number of location of selected piece (-1) */
     white_pieces[location] -= 1;
+
+    /* if player is collecting (see is_available_movement function )*/
     if(movement + location > 25) {
       white_pieces[25] += 1;
     }
     else {
+      /* increase piece number of new location */
       white_pieces[location + movement] += 1;
+
+      /* if there is an enemy piece, then hit it */
       if(black_pieces[location + movement] == 1) {
 	black_pieces[location + movement] = 0;
 	black_pieces[25] += 1;
@@ -867,19 +957,30 @@ void move_piece(int black_pieces [], int white_pieces [],
 void update_scores(int collected_black_pieces, int collected_white_pieces,
 		   int *black_score, int *white_score, int *turn)
 {
+  /* updates scores */
+
+  /* check collected black pieces */
   if(collected_black_pieces == 15) {
+    /* increase black score */
     *black_score += 1;
     printf("Black");
-    if(collected_white_pieces == 0) /* mars */
+    /* check mars: double game points */
+    if(collected_white_pieces == 0)
       *black_score += 1;
+
+    /* next game's first turn will be black's */
     *turn = 0;
   }
 
+  /* check collected white pieces*/
   if(collected_white_pieces == 15) {
+    /* increase white score */
     *white_score += 1;
     printf("White");
-    if(collected_black_pieces == 0) /* mars */
+    /* check mars: double game points */
+    if(collected_black_pieces == 0)
       *white_score += 1;
+    /* next game's first turn will be white's */
     *turn = 1;
   }
 
