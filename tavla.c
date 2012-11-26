99b1787ad45e594fd02e12b3d6e13e72ae7fcab0
Ilker Kesen <ilkerksn@gmail.com>
2012-11-26 03:54:32 +0200

    copyright added

--- tavla.c
+++ tavla.c
@@ -1,10 +1,36 @@
+/* -------------------------------- COPYING --------------------------------- */
+
+/*
+ *   Copyright (C) 2012 by İlker Kesen <ilkerksn[at]gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/* --------------------------------- ABOUT ---------------------------------- */
+
 /*
  * Tavla (backgammon) game with text-based ui
  * Author: İlker Kesen
  * E-mail: keseni _at_ itu.edu.tr
  * Number: 040100411
- *
- * Documentation
+ */
+
+/* ----------------------------- DOCUMENTATION ------------------------------ */
+
+/*
  *   Coding Standarts
  *     1. Use under_score instead of camelCase.
  *     2. Set maximum row to 80 character.
